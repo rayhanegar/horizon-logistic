826c3f1219610d192218eecc58e3245ec585b5c0
Rayhan Egar <rresnn@gmail.com>
2023-11-16 15:41:20 +0700

    Update TestClass.java
    Reply Wayan

--- HorizonLogistic/src/horizonlogistic/TestClass.java
+++ HorizonLogistic/src/horizonlogistic/TestClass.java
@@ -11,5 +11,6 @@ package horizonlogistic;
 public class TestClass {
     public static void main(String[] args){
         System.out.println("Ini Wayan, test");
+        System.out.println("Ini Egar, membalas test Wayan");
     }
 }
